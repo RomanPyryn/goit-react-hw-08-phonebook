62f09e0eef5c8f1c2cc56a2aa7a63f4f1222ca75
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-08-11 13:25:35 +0300

    Add items by redux

--- src/components/Phonebook/index.jsx
+++ src/components/Phonebook/index.jsx
@@ -1,77 +1,93 @@
-import { useState, useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { nanoid } from 'nanoid'
-import ContactForm from "./ContactForm";
-import Filter from "./Filter";
-import ContactList from "./ContactList";
-import { Container, FormBox, ContactsBox } from "./Phonebook.styled"
-import { setFilter } from "redux/filter";
+import { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+import { Container, FormBox, ContactsBox } from './Phonebook.styled';
+import { setFilter } from 'redux/filter';
+import { addItem, removeItem } from 'redux/items';
 
 export default function Phonebook() {
-    const filter = useSelector(state => state.filter.value);
-    const dispatch = useDispatch();
-    const [contacts, setContacts] = useState([]);
-    const isFirstRender = useRef(true);
+  const filter = useSelector(state => state.filter.value);
+  const contacts = useSelector(state => state.items.contacts);
+  const dispatch = useDispatch();
+  const isFirstRender = useRef(true);
 
-    useEffect(() => { 
-        const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+//   useEffect(() => {
+//     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
 
-        if (parsedContacts) {
-            setContacts(parsedContacts);
-        };
-    }, []);
+//     if (parsedContacts) {
+//       // setContacts(parsedContacts);
+//     }
+//   }, []);
 
-    useEffect(() => {
-        if (isFirstRender.current) {
-            isFirstRender.current = false;
-            return;
-        };
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }, [contacts]);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const form = e.currentTarget;
-        const inputName = form.elements.name.value;
-        const inputNamber = form.elements.number.value;
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const inputName = form.elements.name.value;
+    const inputNamber = form.elements.number.value;
 
-        if (contacts.find(contact => contact.name.toLowerCase() === inputName.toLocaleLowerCase())) {
-            form.reset();
-            return window.alert(`${inputName} is already in contacts.`);
-        };
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === inputName.toLocaleLowerCase()
+      )
+    ) {
+      form.reset();
+      return window.alert(`${inputName} is already in contacts.`);
+    }
 
-        const contactObj = {id: nanoid(), name: inputName, number: inputNamber};
-        setContacts(prevState => [...prevState, contactObj]);
-        form.reset();
-    };
+    const contactObj = { id: nanoid(), name: inputName, number: inputNamber };
+    dispatch(addItem(contactObj));
+    form.reset();
+  };
 
-    const deleteContact = (contactId) => {
-        setContacts(contacts.filter(contact => contact.id !== contactId));
-    };
+  const deleteContact = contactId => {
+    dispatch(removeItem(contactId));
+  };
 
-    const changeFilter = (e) => {
-        dispatch(setFilter(e.currentTarget.value));
-    };
+  const changeFilter = e => {
+    dispatch(setFilter(e.currentTarget.value));
+  };
 
-    const getfiltredContacts = () => {
-        return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()),);
-    };
-    
-    return (
-        <Container>
-                <h1>Phonebook</h1>
-                <FormBox>
-                    <ContactForm
-                        onSubmitForm={handleSubmit}
-                        inputName={"Name"}
-                        inputNumber={"Number"}
-                        buttonName={"Add cotact"} />   
-                </FormBox>
-                    <h2>Contacts</h2>
-                <ContactsBox>
-                    <Filter inputName={"Find contacts by name"} value={filter} onChange={changeFilter} />
-                    <ContactList contacts={getfiltredContacts()} buttonName={"Delete"} onBtnClick={deleteContact} />
-                </ContactsBox>
-            </Container>
+  const getfiltredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-};
+  };
+
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <FormBox>
+        <ContactForm
+          onSubmitForm={handleSubmit}
+          inputName={'Name'}
+          inputNumber={'Number'}
+          buttonName={'Add cotact'}
+        />
+      </FormBox>
+      <h2>Contacts</h2>
+      <ContactsBox>
+        <Filter
+          inputName={'Find contacts by name'}
+          value={filter}
+          onChange={changeFilter}
+        />
+        <ContactList
+          contacts={getfiltredContacts()}
+          buttonName={'Delete'}
+          onBtnClick={deleteContact}
+        />
+      </ContactsBox>
+    </Container>
+  );
+}
--- src/redux/items.js
+++ src/redux/items.js
@@ -1,23 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [],
+  contacts: [],
 };
 
 export const itemsSlice = createSlice({
     name: 'items',
     initialState,
     reducers: {
-        setItems: (state, action) => {
-            state.value += action.payload
+        addItem: (state, action) => {
+            state.contacts = [...state.contacts, action.payload];
         },
-        removeItem: (state, action) => {
-            state.value += action.payload
+        removeItem: (state, {payload: id}) => {
+            state.contacts = state.contacts.filter(contact => contact.id !== id) 
         },
     },
 });
 
 // Action creators are generated for each case reducer function
-export const { setItems, removeItem } = itemsSlice.actions;
+export const { addItem, removeItem } = itemsSlice.actions;
 
 export default itemsSlice.reducer;
