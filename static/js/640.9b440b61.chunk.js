80c13010d5d75c41dc42fc79eee0788cb34f8dc1
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-08-28 15:39:05 +0000

    Deploying to gh-pages from @ RomanPyryn/goit-react-hw-08-phonebook@bb18d56e8e409f9a167eba0c47731c30382f3ab3 🚀

---
+++ static/js/640.9b440b61.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[640],{7640:function(e,t,a){a.r(t);var o=a(184);t.default=function(){return(0,o.jsx)("p",{children:"Page not found. Please, go back to the homepage"})}}}]);
+//# sourceMappingURL=640.9b440b61.chunk.js.map
---
+++ static/js/742.9db19b86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[742],{2742:function(e,t,r){r.r(t);var s=r(184);t.default=function(){return(0,s.jsx)(s.Fragment,{children:(0,s.jsxs)("form",{action:"",children:[(0,s.jsxs)("label",{htmlFor:"",children:["Email",(0,s.jsx)("input",{type:"email"})]}),(0,s.jsxs)("label",{htmlFor:"",children:["Password",(0,s.jsx)("input",{type:"password"})]}),(0,s.jsx)("button",{type:"submit",children:"Log in"})]})})}}}]);
+//# sourceMappingURL=742.9db19b86.chunk.js.map
